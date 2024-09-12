0653f1379f91a7cbd32f858c0199983e04ce3f73
Martin <martin@marshtrio.co.uk>
2024-09-12 03:15:23 +0100

    wip

--- main.go
+++ main.go
@@ -69,27 +69,30 @@ func computeHelm(d *nmeaData, mux *nmea_mux.NmeaMux ){
 		d.pd = 1
 	}
 
-	fmt.Printf("data.mode: %s \n", d.mode)
-	fmt.Printf("data.pi: %4.2f \n", d.pi)
-	
+	//fmt.Printf("data.mode: %s \n", d.mode)
+	//fmt.Printf("data.pi: %4.2f \n", d.pi)
+	d.esp_data_ok = true
+	d.mode = "1"
+	d.pd = 0
 
 	if d.mode == "1" {
 		if d.esp_data_ok {
-			error := relative180(d.esp_heading - d.desired_heading)
-			
-			change := (error - d.last_error)
+			course_error:= relative180(d.esp_heading - d.desired_heading)
+			fmt.Printf("error: %5.1f  d.gain: %.0f\n", course_error, d.gain)
+			change := (course_error - d.last_error)
 			d.last_error += change/20
 			
-			d.integral +=  error *  d.pi/30000.0;
+			d.integral +=  course_error *  d.pi/30000.0;
 			if (d.integral > 15.0){
 				d.integral = 15.0;
 			} else if (d.integral < -15.0){
 				d.integral = -15.0;
 			}
 
-			helm_pos := (error + d.integral - change*d.pd) * d.gain * 3
+			helm_pos := (course_error + d.integral - change*d.pd) * d.gain * 3
 			fmt.Printf("helm: %5.1f \n", helm_pos)
-			//to_helm <- helm_pos
+			str, _ := moveTo(helm_pos, d, mux)
+			fmt.Printf("Send 0183: %s\n", str)
 
 		}
 
@@ -107,6 +110,17 @@ func  relative180(dif float32) float32{
 	return dif
 }
 
+func moveTo(position float32, d *nmeaData, mux *nmea_mux.NmeaMux )(string, error){
+	handle := mux.Processors["main_processor"].GetNmeaHandle()
+	handle.Nmea_mu.Lock()           // must lock and unlock on function end
+	defer handle.Nmea_mu.Unlock()
+	data := handle.Nmea.GetMap()
+	data["helm_helm"] = fmt.Sprintf("%.0f", position)
+	data["helm_reset"] = "0"
+	str, err :=handle.Nmea.WriteSentencePrefixVar("PX","xs2", "helm_")
+	return str, err
+}
+
 func getNmeaData(d *nmeaData, mux *nmea_mux.NmeaMux ){
 	ok := true
 	handle := mux.Processors["main_processor"].GetNmeaHandle()
@@ -120,10 +134,10 @@ func getNmeaData(d *nmeaData, mux *nmea_mux.NmeaMux ){
 	d.gain, ok =  getFloat32(d.gain, data["esp_auto_gain"], 0, ok)
 	d.pi, ok = getFloat32(d.pi, data["esp_auto_pi"], 0, ok)
 	d.pd, ok = getFloat32(d.pd, data["esp_auto_pd"], 0, ok)
-	d.esp_heading, ok = getFloat32(d.esp_heading, data["esp_hdm"], -2, ok)	
-	d.desired_heading, ok = getFloat32(d.desired_heading, data["esp_set_hdm"], -2, ok)
+	d.esp_heading, ok = getFloat32(d.esp_heading, data["esp_hdm"], -3, ok)	
+	d.desired_heading, ok = getFloat32(d.desired_heading, data["esp_set_hdm"], -3, ok)
 	d.esp_data_ok = ok
-	d.compass_heading, d.cp_data_ok = getFloat32(d.compass_heading, data["cp_hdm"], -2, true)	
+	d.compass_heading, d.cp_data_ok = getFloat32(d.compass_heading, data["cp_hdm"], -3, true)	
 
 }
 
@@ -131,6 +145,7 @@ func getFloat32(current float32, data string, end int, e bool) (float32, bool){
 	l := len(data) + end
 	
 	if l > 0 {
+		fmt.Printf("conv %s end %d\n", data[:l], end )
 		f64, err := strconv.ParseFloat(data[:l], 32)
 		if err != nil{
 			return current, false
